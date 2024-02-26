a93ff397d8a1e4c0b574ecd066cbb8b516917c2c
MacaCodes <gibsonmacauley@gmail.com>
2024-02-26 12:13:26 +0000

    winner

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://macacodes.github.io/todofinal/",
-  plugins: [react()],
-  base: '/vite-deploy-demo/'
-})
+  base: "/todofinal/", // Set the base path to '/todofinal/'
+  plugins: [react()]
+});
